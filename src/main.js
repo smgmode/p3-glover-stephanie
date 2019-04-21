cb16c3a43f23f4f9a3c47b763f6d40b3e9b51fd1
Stephanie Glover <stephanie.glover@ufl.edu>
2019-04-20 23:08:01 -0700

    Add the E6 version of the fortune generator to fourth page

--- src/main.js
+++ src/main.js
@@ -1,77 +1,85 @@
 //Global variables
-/*
+const one = 1;
+const two = 2;
+const three = 3;
+const four = 4;
+const pageone = document.getElementsByClassName('page1')[0];
+const pagetwo = document.getElementsByClassName('page2')[0];
+const pagethree = document.getElementsByClassName('page3')[0];
+const pagefour = document.getElementsByClassName('page4-row')[0];
+const button = document.getElementsByTagName('button')[0];
+
+
+
 //page 1
-const redbox = getElementsByClassName('eyebox');
-const picone = 1;
-const orangebox = getElementsByClassName('nailbox');
-const pictwo = 2;
-const aquabox = getElementsByClassName('lipbox');
-const picthree = 3;
-const bluebox = getElementsByClassName('makebox');
-const picfour = 4;
-*/
+const redbox = document.getElementsByClassName('eyebox');
+const orangebox = document.getElementsByClassName('nailbox');
+const aquabox = document.getElementsByClassName('lipbox');
+const bluebox = document.getElementsByClassName('makebox');
 
 
 //page 2
 const onebox = document.getElementsByClassName('number1');
-const one = 1;
 const twobox = document.getElementsByClassName('number2');
-const two = 2;
 const threebox = document.getElementsByClassName('number3');
-const three = 3;
 const fourbox = document.getElementsByClassName('number4');
-const four = 4;
 
-/*
+
 //page3
-const redonebox = getElementsByClassName('number-1');
-const rone = 1;
-const orangetwobox = getElementsByClassName('number-2');
-const otwo = 2;
-const aquathreebox = getElementsByClassName('number-3');
-const athree = 3;
-const bluefourbox = getElementsByClassName('number-4');
-const bfour = 4;
-*/
+const redonebox = document.getElementsByClassName('number-1');
+const orangetwobox = document.getElementsByClassName('number-2');
+const aquathreebox = document.getElementsByClassName('number-3');
+const bluefourbox = document.getElementsByClassName('number-4');
 
-/*
 //functionality page 1
+function changeLayer () {
+  pagetwo.classList.remove('hide');
+  pageone.classList.add('hide');
+}
 function loopThroughFirst() {
   const changeColor = () => this.classList.toggle('red');
-  for (let i = 0; i < picone * 2; i++) {
-    setTimeout(changeColor, i * 1000)
+  for (let i = 0; i < one * 2; i++) {
+    setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayer, one * 2000);
 };
 
 function loopThroughSecond() {
   const changeColor = () => this.classList.toggle('red');
-  for (let i = 0; i < pictwo * 2; i++) {
+  for (let i = 0; i < two * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayer, two * 2000);
 };
 
 function loopThroughThird() {
   const changeColor = () => this.classList.toggle('red');
-  for (let i = 0; i < picthree * 2; i++) {
+  for (let i = 0; i < three * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayer, three * 2000);
 };
 
 function loopThroughFourth() {
   const changeColor = () => this.classList.toggle('red');
-  for (let i = 0; i < picfour * 2; i++) {
+  for (let i = 0; i < four * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayer, four * 2000);
 };
-*/
 
 
 //functionality Page 2
+function changeLayertwo () {
+  pagethree.classList.remove('hide');
+  pagetwo.classList.add('hide');
+}
 function loopThroughOne() {
   const changeColor = () => this.classList.toggle('red');
   for (let i = 0; i < one * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayertwo, one * 2000);
 };
 
 
@@ -80,6 +88,7 @@ function loopThroughTwo() {
   for (let i = 0; i < two * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayertwo, two * 2000);
 };
 
 
@@ -88,6 +97,7 @@ function loopThroughThree() {
   for (let i = 0; i < three * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayertwo, three * 2000);
 };
 
 
@@ -96,50 +106,80 @@ function loopThroughFour() {
   for (let i = 0; i < four * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayertwo, four * 2000);
 };
 
 
 
-/*
 //funcionality Page 3
+function changeLayerThree () {
+  pagefour.classList.remove('hide');
+  pagethree.classList.add('hide');
+}
+
 function loopThroughFive() {
-  const changeColorOne = () => this.classList.toggle('red');
-  for (let i = 0; i < rone * 2; i++) {
+  const changeColor = () => this.classList.toggle('red');
+  for (let i = 0; i < one * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayerThree, one * 2000);
 };
 
 
 function loopThroughSix() {
-  const changeColorTwo = () => this.classList.toggle('red');
-  for (let i = 0; i < otwo * 2; i++) {
+  const changeColor = () => this.classList.toggle('red');
+  for (let i = 0; i < two * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayerThree, two * 2000);
 };
 
 
 function loopThroughSeven() {
-  const changeColorThree = () => this.classList.toggle('red');
-  for (let i = 0; i < athree * 2; i++) {
+  const changeColor = () => this.classList.toggle('red');
+  for (let i = 0; i < three * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayerThree, three * 2000);
 };
 
 
 function loopThroughEight() {
-  const changeColorFour = () => this.classList.toggle('red');
-  for (let i = 0; i < bfour * 2; i++) {
+  const changeColor = () => this.classList.toggle('red');
+  for (let i = 0; i < four * 2; i++) {
     setTimeout(changeColor, i * 1000);
   }
+  setTimeout(changeLayerThree, four * 2000);
 };
-*/
-/*
+
+//page four functionality fortune generator
+const ourRandomFortuneItem =document.createElement('p');
+const fortuneItemWrapper = document.getElementById('generator-results');
+fortuneItemWrapper.appendChild(ourRandomFortuneItem);
+
+const placeholderText = document.createTextNode('');
+ourRandomFortuneItem.appendChild(placeholderText);
+
+const fortuneItems = ['Beauty is in the Eyes of the Beholder', 'Keeping it 100', 'Own your beauty', 'Go to Thee Beauty Bar', 'One Two Three Four Pick Up the Make Up Off the Floor', 'Love Yourself',
+'Everything Has Beauty', 'Life Life and Enjoy It', 'Beauty of Expression is so Akin to the Voice of the Sea', 'I Always Say Kindness is the Greatest Beauty That You Can Have', 'The Absence of Flaw in Beauty is Itself a Flaw'];
+
+const loopThroughNine = function() {
+  fortuneItems.sort(function(a, b){return 0.5 - Math.random()});
+  const ourRandomFortuneItemFiller = document.createTextNode(fortuneItems[0]);
+  ourRandomFortuneItem.appendChild(ourRandomFortuneItemFiller);
+  const ourButtonSection = document.getElementById('generator-button');
+  ourButtonSection.removeChild(button);
+  ourRandomFortuneItem.classList.add('big-and-bold');
+};
+
+button.onclick = loopThroughNine;
+
 //event listener page 1
 redbox[0].addEventListener('click',loopThroughFirst);
 orangebox[0].addEventListener('click',loopThroughSecond);
 aquabox[0].addEventListener('click',loopThroughThird);
 bluebox[0].addEventListener('click',loopThroughFourth);
-*/
+
 
 
 //event listener page 2
@@ -148,10 +188,9 @@ twobox[0].addEventListener('click',loopThroughTwo);
 threebox[0].addEventListener('click',loopThroughThree);
 fourbox[0].addEventListener('click',loopThroughFour);
 
-/*
+
 //event listener page3
 redonebox[0].addEventListener('click',loopThroughFive);
 orangetwobox[0].addEventListener('click',loopThroughSix);
 aquathreebox[0].addEventListener('click',loopThroughSeven);
-bluefourbox[0].addEventListener('click',loopThroughEight);
-*/
+bluefourbox[0].addEventListener('click', loopThroughEight);
